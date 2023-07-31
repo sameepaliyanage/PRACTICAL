57a14eb885f75675833fd6ec4dd13ddaf576b2b9
sameepaliyanage <140710190+sameepaliyanage@users.noreply.github.com>
2023-07-31 10:19:44 +0530

    Add files via upload

---
+++ Lab 06-27156/Lab 06 Question 06/Lab 06 Question 06/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_06_Question_06
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[10];
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
