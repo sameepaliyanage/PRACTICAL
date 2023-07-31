6966878abd8632eea2f1dca701ef9f27f5fba563
sameepaliyanage <140710190+sameepaliyanage@users.noreply.github.com>
2023-07-31 10:20:46 +0530

    Add files via upload

---
+++ Lab 07-27156/Lab 07 Question 07/Lab 07 Question 07/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_07_Question_07
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int arraySize = Convert.ToInt32(Console.ReadLine());
+
+            int[] array1 = new int[arraySize];
+            int[] array2 = new int[arraySize];
+
+            for (int i = 0; i < arraySize; i++) 
+            {
+                array1[i] = Convert.ToInt32(Console.ReadLine());
+                array2[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            int scalarSum = 0;
+            for (int i = 0; i < arraySize; i++)
+            {
+                scalarSum += array1[i] + array2[i];
+            }
+            int[] vectorSum = new int[arraySize];
+            for (int i = 0; i < arraySize; i++)
+            {
+                vectorSum[i] = array1[i] + array2[i];
+            }
+            int[] vectorProduct = new int[arraySize];
+            for (int i = 0; i < arraySize; i++)
+            {
+                vectorProduct[i] = array1[i] * array2[i];
+            }
+            int scalarProduct = 0;
+            for (int i = 0; i < arraySize; i++)
+            {
+                scalarProduct += array1[i] * array2[i];
+            }
+            Console.WriteLine("The scalar sum is " + scalarSum);
+            Console.WriteLine("The vector sum is " + vectorSum);
+            Console.WriteLine("The vector product is " + vectorProduct);
+            Console.WriteLine("The scalar product is " + scalarProduct);
+            Console.ReadKey();
+
+
+        }
+    }
+}
---
+++ Lab 07-27156/Lab 07 Question 08/Lab 07 Question 08/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_07_Question_08
+{
+    class Animal
+    {
+        public void printAnimal()
+        {
+            Console.WriteLine("I am an animal");
+        }
+    }
+    class Dog:Animal
+    {
+        public void printDog()
+        {
+            Console.WriteLine("I have four legs");
+        }
+    }
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Dog dog = new Dog();
+            dog.printAnimal();
+            dog.printDog();
+            Console.ReadLine();
+        }
+    }
+}
